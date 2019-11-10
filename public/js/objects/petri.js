a6244a8d8d22183b287e357decdf63acee8ea15f
DenyStark <denyskrolevetskyi@gmail.com>
2019-11-11 00:07:27 +0200

    Feat: Select object view

--- public/js/objects/petri.js
+++ public/js/objects/petri.js
@@ -8,10 +8,25 @@ class PetriObject {
     const object = `
       <div class="sandbox-item text-center draggable" id="item-${id}">
         P${petriCounter}<br>
-        <svg width="60" height="60">
-          <circle cx="30" cy="30" r="22.5" style="fill:white" />
-        </svg>
+        <div class="sandbox-object">
+          <svg width="60" height="60">
+            <circle cx="30" cy="30" r="22.5" style="fill:white" />
+          </svg>
+        </div>
       </div>`;
     $('#sandbox').append(object);
+
+    $(`#item-${id}`).click(this.select);
+    $(document).on('click', e => {
+      if (!$(e.target).closest(`#item-${id}`).length) this.deselect();
+    });
+  }
+
+  select() {
+    $($('.sandbox-object', `#${this.id}`)[0]).addClass('select');
+  }
+
+  deselect() {
+    $($('.sandbox-object', `#item-${this.id}`)[0]).removeClass('select');
   }
 };
--- public/js/objects/transition.js
+++ public/js/objects/transition.js
@@ -8,10 +8,25 @@ class TransitionObject {
     const object = `
       <div class="sandbox-item text-center draggable" id="item-${id}">
         T${transitionCounter}<br>
-        <svg width="60" height="60">
-          <rect x="27" y="6" width="6" height="48" style="fill:#333333" />
-        </svg>
+        <div class="sandbox-object">
+          <svg width="60" height="60">
+            <rect x="27" y="6" width="6" height="48" style="fill:#333333" />
+          </svg>
+        </div>
       </div>`;
     $('#sandbox').append(object);
+
+    $(`#item-${id}`).click(this.select);
+    $(document).on('click', e => {
+      if (!$(e.target).closest(`#item-${id}`).length) this.deselect();
+    });
+  }
+
+  select() {
+    $($('.sandbox-object', `#${this.id}`)[0]).addClass('select');
+  }
+
+  deselect() {
+    $($('.sandbox-object', `#item-${this.id}`)[0]).removeClass('select');
   }
 };
