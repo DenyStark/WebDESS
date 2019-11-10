80fd660c56566b60f08bc9f5add975754de04dd6
DenyStark <denyskrolevetskyi@gmail.com>
2019-11-10 22:04:07 +0200

    Feat: Transition objects

--- public/js/net.js
+++ public/js/net.js
@@ -1,7 +1,14 @@
 const petriObjects = [];
+const transitionObjects = [];
 
 function createPetri() {
   const id = (Math.round(Math.random() * 1e12)).toString(26);
   const object = new PetriObject(id);
   petriObjects.push(object);
+}
+
+function createTransition() {
+  const id = (Math.round(Math.random() * 1e12)).toString(26);
+  const object = new TransitionObject(id);
+  transitionObjects.push(object);
 }
--- public/js/objects/transition.js
+++ public/js/objects/transition.js
@@ -1,15 +1,15 @@
-let petriCounter = 0;
+let transitionCounter = 0;
 
-class PetriObject {
+class TransitionObject {
   constructor(id) {
     this.id = id;
-    petriCounter++;
+    transitionCounter++;
 
     const object = `
       <div class="sandbox-item text-center draggable" id="item-${id}">
-        P${petriCounter}<br>
+        T${transitionCounter}<br>
         <svg width="60" height="60">
-          <circle cx="30" cy="30" r="22.5" style="fill:white" />
+          <rect x="27" y="6" width="6" height="48" style="fill:#333333" />
         </svg>
       </div>`;
     $('#sandbox').append(object);
