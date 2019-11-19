9b9b6470debe3fc096a4eea6d048e27deb1638f4
DenyStark <denyskrolevetskyi@gmail.com>
2019-11-20 01:17:23 +0200

    Feat: Use axios

--- public/js/files-manager.js
+++ public/js/files-manager.js
@@ -4,25 +4,25 @@ const processFaulure = (faulure) => {
 };
 
 async function loadList() {
-  const { list } = await $.get('/storage/list');
+  const { list } = await axios.get('/storage/list');
   return list || [];
 }
 
 async function loadFile(title) {
   const query = { title };
-  const { file } = await $.get('/storage/file', query).catch(processFaulure);
+  const { file } = await axios.get('/storage/file', query).catch(processFaulure);
   return file || {};
 }
 
 async function updateFile(title, data) {
   const body = { title, data };
-  await $.post('/storage/update', body).catch(processFaulure);
+  await axios.post('/storage/update', body).catch(processFaulure);
   alert('The file is successfully update.');
 }
 
 async function createFile(title, isUpdate, data = {}) {
   const body = { title, data };
-  await $.post('/storage/create', body).catch((faulure) => {
+  await axios.post('/storage/create', body).catch((faulure) => {
     const { code, message } = faulure.responseJSON;
     if (code === 73401 && isUpdate) updateFile(title, data);
     else alert(`Error: ${code}: ${message}`);
@@ -33,7 +33,7 @@ async function createFile(title, isUpdate, data = {}) {
 
 function deleteFile(title) {
   const body = { title };
-  $.post('/storage/delete', body).then(() => {
+  axios.post('/storage/delete', body).then(() => {
     alert('The file is successfully delete.');
   }, processFaulure);
 }
