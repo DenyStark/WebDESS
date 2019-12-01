c843d3d1b540a5dc12be98e24062eea0a8a73167
DenyStark <denyskrolevetskyi@gmail.com>
2019-12-01 13:22:10 +0200

    Ref: files-manager script

--- public/js/files-manager.js
+++ public/js/files-manager.js
@@ -1,29 +1,30 @@
-const processFaulure = (faulure) => {
-  const { code, message, data } = faulure.responseJSON;
+const processError = error => {
+  const { code, message, data } = error.responseJSON;
   alert(`Error: ${code}: ${message || data}`);
 };
 
-async function loadList() {
-  const { list } = (await axios.get('/storage/list')).data;
-  return list || [];
+async function loadList(type = 'Net') {
+  const params = { type };
+  const payload = (await axios.get('/storage/list', { params }));
+  return payload.data.list || [];
 }
 
-async function loadFile(title) {
-  const params = { title };
-  const { data } = await axios.get('/storage/file', { params }).catch(processFaulure);
-  return data.file || {};
+async function loadFile(title, type = 'Net') {
+  const params = { title, type };
+  const payload = await axios.get('/storage/file', { params }).catch(processError);
+  return payload.data.file || {};
 }
 
-async function updateFile(title, data) {
-  const body = { title, data };
-  await axios.post('/storage/update', body).catch(processFaulure);
+async function updateFile(title, data, type = 'Net') {
+  const body = { title, type, data };
+  await axios.post('/storage/update', body).catch(processError);
   alert('The file is successfully update.');
 }
 
-async function createFile(title, isUpdate, data = {}) {
-  const body = { title, data };
-  await axios.post('/storage/create', body).catch((faulure) => {
-    const { code, message } = faulure.responseJSON;
+async function createFile(title, isUpdate, data = {}, type = 'Net') {
+  const body = { title, type, data };
+  await axios.post('/storage/create', body).catch(error => {
+    const { code, message } = error.response.data;
     if (code === 73401 && isUpdate) updateFile(title, data);
     else alert(`Error: ${code}: ${message}`);
   });
@@ -31,9 +32,9 @@ async function createFile(title, isUpdate, data = {}) {
   alert('The file is successfully save.');
 }
 
-function deleteFile(title) {
-  const body = { title };
+function deleteFile(title, type = 'Net') {
+  const body = { title, type };
   axios.post('/storage/delete', body).then(() => {
     alert('The file is successfully delete.');
-  }, processFaulure);
+  }, processError);
 }
